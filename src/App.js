e5923527354852ace3e4280c9f66598d1be840c1
Cai heng <ianyaheng822@gmail.com>
2020-01-15 21:57:32 +1100

    useState with cleanup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import HookCounterArray from './components/HookCounterArray';
 import HookCounterObject from './components/HookCounterObject';
 import HookCounterOne from './components/HookCounterOne';
-
-
+import HookMouse from './components/HookMouse';
 import './App.css';
+import MouseContainer from './components/MouseContainer';
 
 function App() {
   return (
     <div>
-      <HookCounterOne/>
+      <MouseContainer/>
     </div>
 
    
