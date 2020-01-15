b098922865a77c4bd97d238b1dd7a76ebb720b2e
Cai heng <ianyaheng822@gmail.com>
2020-01-15 11:58:33 +1100

    userState with object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import HookCounterArray from './components/HookCounterArray';
+import HookCounterObject from './components/HookCounterObject';
 
 import './App.css';
 
 function App() {
   return (
     <div>
-
-
+      <HookCounterObject/>
     </div>
 
    
---
+++ src/components/HookCounterObject.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+function HookCounterObject() {
+
+ const [name,setName]=useState({
+   firstName:'',
+   lastName:''
+ })
+  return (
+    <div>
+      <form action="">
+        <input type="text" value={name.firstName} onChange={e=>setName({...name,firstName:e.target.value})}/>
+        <input type="text" value={name.lastName} onChange={e=>setName({...name,lastName:e.target.value})}/>
+        <h2>You firstName is {name.firstName}</h2>
+        <h2>You lastName is {name.lastName}</h2>
+        <h2>{JSON.stringify(name)}</h2>
+
+
+      </form>
+
+
+      
+    </div>
+  )
+}
+
+export default HookCounterObject
