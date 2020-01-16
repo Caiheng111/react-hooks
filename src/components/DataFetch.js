f702620be0fe4595b2cddf37b84a8a162ce5fd74
Cai heng <ianyaheng822@gmail.com>
2020-01-16 12:53:36 +1100

    useEffect fetch data2

--- src/components/DataFetch.js
+++ src/components/DataFetch.js
@@ -2,27 +2,28 @@ import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 
 function DataFetch() {
-  const [posts, setPosts]=useState([]);
+  const [post, setPost]=useState({});
+  const [id, setId]=useState(1);
 
   useEffect(()=>{
-    axios.get('https://jsonplaceholder.typicode.com/posts')
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
     .then(res=>{
       console.log(res);
-      setPosts(res.data)
+      setPost(res.data)
       
     })
     .catch(err=>{
       console.log(err);
       
     })
-  },[])
+  },[id])
   return (
     <div>
+      <input type="text" value={id} onChange={e=>setId(e.target.value)}/>
+  <h2>{post.title}</h2>
         <ul>
           {
-            posts.map(post=>(
-              <li key={post.id}>{post.title}</li>
-            ))
+            
           }
         </ul>
     </div>
